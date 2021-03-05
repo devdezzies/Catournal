d4ba65a01e9c8774a6e2c3a404698ecd5cb543d0
MrFluxy <devdezzies@users.noreply.github.com>
2021-03-05 16:49:04 +0700

    Hello blogs

--- src/components/Gitalk/newes.js
+++ src/components/Gitalk/newes.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import Plyr from "react-plyr";
-import {Helmet} from "react-helmet";
+import { Helmet } from "react-helmet";
+import Button from '../_ui/Button';
+import styled from "@emotion/styled";
+import { Link } from 'gatsby';
 const styles = {
     fontFamily: "sans-serif",
     textAlign: "center"
 };
 
+const WorkLink = styled(Link)`
+    margin-top: 3em;
+    display: block;
+    text-align: center;
+    margin-bottom: 3em;
+`
+
 const Newes = () => {
 
     const [
-        hasmounted, 
+        hasmounted,
         setHasMounted
     ] = React.useState(false);
 
@@ -23,14 +33,18 @@ const Newes = () => {
 
     return (
         <div style={styles}>
-        <Helmet>
-            <link rel="stylesheet" href="https://cdn.plyr.io/2.0.18/plyr.css"/>
-        </Helmet>
+            <Helmet>
+                <link rel="stylesheet" href="https://cdn.plyr.io/2.0.18/plyr.css" />
+            </Helmet>
             <Plyr
                 type="video"
-                url="https://upload.wikimedia.org/wikipedia/commons/transcoded/c/c0/Big_Buck_Bunny_4K.webm/Big_Buck_Bunny_4K.webm.480p.webm"
+                url="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-1080p.mp4"
             />
-            <h2>Start editing to see some magic happens {"\u2728"}</h2>
+            <WorkLink to={"/blog"}>
+                <Button className="Button--secondary">
+                    Hi! Do you interesting about this? watch more {'\u2728'}
+                </Button>
+            </WorkLink>
         </div>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,8 @@ const Hero = styled("div")`
     padding-top: 2.5em;
     padding-bottom: 3em;
     margin-bottom: 6em;
-    max-width: 830px;
+    max-width: 810px;
+   
 
     @media(max-width:${dimensions.maxwidthMobile}px) {
        margin-bottom: 3em;
