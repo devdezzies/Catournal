3264fee9ebe7aff0b7c945280b3b13829dc74877
MrFluxy <devdezzies@users.noreply.github.com>
2021-03-14 19:19:44 +0700

    Forum SEO

--- src/pages/forum.js
+++ src/pages/forum.js
@@ -1,19 +1,26 @@
 import React from 'react'
 import Layout from "components/Layout"
+import Helmet from "react-helmet"
 import '@suziwen/gitalk/dist/gitalk.css'
 import Gitalk from 'gatsby-plugin-gitalk'
 
-const Forum = ({uid}) => {
+const Forum = ({ uid }) => {
 
     let gitalkConfig = {
         id: `${uid}`,
-        
-      }
+
+    }
 
     return (
-    <Layout>
-        <Gitalk options={gitalkConfig}/>
-    </Layout>
+        <>
+            <Helmet
+                title={`Catournal Forum`}
+                titleTemplate={`Hello, welcome to Catournal Forum`}
+            />
+            <Layout>
+                <Gitalk options={gitalkConfig} />
+            </Layout>
+        </>
     )
 
 }
