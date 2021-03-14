40d430e39b7cb2d4b5ab5c73bab9f818cacff388
MrFluxy <devdezzies@users.noreply.github.com>
2021-03-14 18:53:25 +0700

    Forum updated

--- src/pages/forum.js
+++ src/pages/forum.js
@@ -3,10 +3,16 @@ import Layout from "components/Layout"
 import '@suziwen/gitalk/dist/gitalk.css'
 import Gitalk from 'gatsby-plugin-gitalk'
 
-const Forum = () => {
+const Forum = ({uid}) => {
+
+    let gitalkConfig = {
+        id: `${uid}`,
+        
+      }
+
     return (
     <Layout>
-        <Gitalk />
+        <Gitalk options={gitalkConfig}/>
     </Layout>
     )
 
